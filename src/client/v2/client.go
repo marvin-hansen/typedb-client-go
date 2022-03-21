40fd34b80063abded47819ba24712f79f36f90e4
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-21 10:06:20 +0800

    Updated client.

--- src/client/v2/client.go
+++ src/client/v2/client.go
@@ -4,24 +4,54 @@ import (
 	"context"
 	pb "github.com/marvin-hansen/go-typedb/proto/core"
 	"google.golang.org/grpc"
-	"time"
+	"log"
+	"sync"
 )
 
 type Client struct {
 	client pb.TypeDBClient
 	config *Config
 	ctx    context.Context
-	cnc    context.CancelFunc
+	mtx    sync.RWMutex
 }
 
-func NewClient(conn *grpc.ClientConn) (*Client, error) {
+func NewClient(conf *Config) (*Client, context.CancelFunc) {
+	dbHost := conf.GetConnectionString()
+
+	conn, cErr := newConnection(dbHost)
+	if cErr != nil {
+		log.Fatal("While trying to dial gRPC at: " + dbHost)
+	}
+
+	dbClient, clErr := newClient(conn)
+	if clErr != nil {
+		log.Fatal("While creating TypeDB client")
+	}
+
+	cancelFn := func() {
+		if err := conn.Close(); err != nil {
+			log.Printf("Error while closing TypeDB connection:%v", err)
+		}
+	}
+
+	return dbClient, cancelFn
+}
+
+func newConnection(target string) (*grpc.ClientConn, error) {
+	conn, err := grpc.Dial(target, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("While trying to dial gRPC")
+	}
+	return conn, err
+}
+
+func newClient(conn *grpc.ClientConn) (*Client, error) {
 	checkConnection(conn, nil)
 	client := pb.NewTypeDBClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	typeDBclient := &Client{
+	ctx := context.Background()
+	typeDBClient := &Client{
 		client: client,
 		ctx:    ctx,
-		cnc:    cancel,
 	}
-	return typeDBclient, nil
+	return typeDBClient, nil
 }
