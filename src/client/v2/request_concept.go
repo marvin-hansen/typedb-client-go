4e52f82663d8b5a4d3919c50101ccb2092f00215
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-22 19:31:38 +0800

    Added request builders for logic manager requests.

--- src/client/v2/request_concept.go
+++ src/client/v2/request_concept.go
@@ -5,13 +5,13 @@ import "github.com/marvin-hansen/go-typedb/proto/common"
 // ConceptManager
 // https://github.com/vaticle/typedb-client-python/blob/master/typedb/common/rpc/request_builder.py
 
-// getConceptManagerTx coverts a ConceptManager_Req into a Transaction_Req
-func getConceptManagerTx(req *common.ConceptManager_Req) *common.Transaction_Req {
+// getConceptTx coverts a ConceptManager_Req into a Transaction_Req
+func getConceptTx(req *common.ConceptManager_Req) *common.Transaction_Req {
 	r := &common.Transaction_Req_ConceptManagerReq{ConceptManagerReq: req}
 	return &common.Transaction_Req{Req: r}
 }
 
-func getConceptManagerPutEntityReq(label string) *common.ConceptManager_Req {
+func getConceptPutEntityReq(label string) *common.ConceptManager_Req {
 	req := &common.ConceptManager_Req_PutEntityTypeReq{}
 	req.PutEntityTypeReq = &common.ConceptManager_PutEntityType_Req{
 		Label: label,
@@ -19,7 +19,7 @@ func getConceptManagerPutEntityReq(label string) *common.ConceptManager_Req {
 	return &common.ConceptManager_Req{Req: req}
 }
 
-func getConceptManagerPutRelationReq(label string) *common.ConceptManager_Req {
+func getConceptPutRelationReq(label string) *common.ConceptManager_Req {
 	req := &common.ConceptManager_Req_PutRelationTypeReq{}
 	req.PutRelationTypeReq = &common.ConceptManager_PutRelationType_Req{
 		Label: label,
@@ -27,7 +27,7 @@ func getConceptManagerPutRelationReq(label string) *common.ConceptManager_Req {
 	return &common.ConceptManager_Req{Req: req}
 }
 
-func getConceptManagerPutAttributeReq(label string, valueType common.AttributeType_ValueType) *common.ConceptManager_Req {
+func getConceptPutAttributeReq(label string, valueType common.AttributeType_ValueType) *common.ConceptManager_Req {
 	req := &common.ConceptManager_Req_PutAttributeTypeReq{}
 	req.PutAttributeTypeReq = &common.ConceptManager_PutAttributeType_Req{
 		Label:     label,
@@ -36,7 +36,7 @@ func getConceptManagerPutAttributeReq(label string, valueType common.AttributeTy
 	return &common.ConceptManager_Req{Req: req}
 }
 
-func getConceptManagerGetThingTypeReq(label string) *common.ConceptManager_Req {
+func getConceptGetThingTypeReq(label string) *common.ConceptManager_Req {
 	req := &common.ConceptManager_Req_GetThingTypeReq{}
 	req.GetThingTypeReq = &common.ConceptManager_GetThingType_Req{
 		Label: label,
@@ -44,7 +44,7 @@ func getConceptManagerGetThingTypeReq(label string) *common.ConceptManager_Req {
 	return &common.ConceptManager_Req{Req: req}
 }
 
-func getConceptManagerGetThingReq(iid string) *common.ConceptManager_Req {
+func getConceptGetThingReq(iid string) *common.ConceptManager_Req {
 	req := &common.ConceptManager_Req_GetThingReq{}
 	req.GetThingReq = &common.ConceptManager_GetThing_Req{
 		Iid: []byte(iid),
---
+++ src/client/v2/request_logic.go
@@ -0,0 +1,33 @@
+package v2
+
+import "github.com/marvin-hansen/go-typedb/proto/common"
+
+// LogicManager
+// https://github.com/vaticle/typedb-client-python/blob/master/typedb/common/rpc/request_builder.py
+
+// getLogicTx coverts a LogicManager_Req into a Transaction_Req
+func getLogicTx(req *common.LogicManager_Req) *common.Transaction_Req {
+	r := &common.Transaction_Req_LogicManagerReq{LogicManagerReq: req}
+	return &common.Transaction_Req{Req: r}
+}
+
+func getLogicPutRuleReq(label, when, then string) *common.LogicManager_Req {
+	req := &common.LogicManager_PutRule_Req{
+		Label: label,
+		When:  when,
+		Then:  then,
+	}
+	return &common.LogicManager_Req{Req: &common.LogicManager_Req_PutRuleReq{PutRuleReq: req}}
+}
+
+func getLogicGetRuleReq(label string) *common.LogicManager_Req {
+	req := &common.LogicManager_GetRule_Req{
+		Label: label,
+	}
+	return &common.LogicManager_Req{Req: &common.LogicManager_Req_GetRuleReq{GetRuleReq: req}}
+}
+
+func getLogicGetAllRulesReq(label string) *common.LogicManager_Req {
+	req := &common.LogicManager_GetRules_Req{}
+	return &common.LogicManager_Req{Req: &common.LogicManager_Req_GetRulesReq{GetRulesReq: req}}
+}
