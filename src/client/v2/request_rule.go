0c6fb467ddfd17421ff9a698c748965702605641
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-23 20:05:10 +0800

    Implemented rule requests.

---
+++ src/client/v2/request_rule.go
@@ -0,0 +1,25 @@
+package v2
+
+import "github.com/marvin-hansen/go-typedb/common"
+
+// Rule
+// 758
+
+// getRuleTx coverts a RuleReq into a Transaction_Req
+func getRuleTx(req *common.Rule_Req, label string) *common.Transaction_Req {
+	req.Label = label
+	r := &common.Transaction_Req_RuleReq{RuleReq: req}
+	return &common.Transaction_Req{Req: r}
+}
+
+func getRuleSetLabelReq(label string, newLabel string) (*common.Transaction_Req, error) {
+	s := &common.Rule_SetLabel_Req{Label: newLabel}
+	req := &common.Rule_Req{Req: &common.Rule_Req_RuleSetLabelReq{RuleSetLabelReq: s}}
+	return getRuleTx(req, label), nil
+}
+
+func getRuleDeleteReq(label string) (*common.Transaction_Req, error) {
+	s := &common.Rule_Delete_Req{}
+	req := &common.Rule_Req{Req: &common.Rule_Req_RuleDeleteReq{RuleDeleteReq: s}}
+	return getRuleTx(req, label), nil
+}
