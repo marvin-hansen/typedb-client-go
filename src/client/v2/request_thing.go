019fd82f0e90003589d6ace7fb0a3b829978741d
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-23 19:34:58 +0800

    Implemented thing requests.

--- src/client/v2/request_thing.go
+++ src/client/v2/request_thing.go
@@ -18,19 +18,58 @@ func convertThingToProto(iid string) *common.Thing {
 	return &common.Thing{Iid: convertStringToByte(iid)}
 }
 
-// getTypeTx coverts a ThingReq into a Transaction_Req
+// getThingTx coverts a ThingReq into a Transaction_Req
 func getThingTx(req *common.Thing_Req, iid string) *common.Transaction_Req {
 	req.Iid = convertStringToByte(iid)
 	r := &common.Transaction_Req_ThingReq{ThingReq: req}
 	return &common.Transaction_Req{Req: r}
 }
 
-func getThingHasReq(iid string, attributeTypes *[]common.Type, onlyKey bool) (*common.Transaction_Req, error) {
+func getThingHasReq(iid string, attributeTypes *[]*common.Type, onlyKey bool) (*common.Transaction_Req, error) {
 
 	if attributeTypes != nil && onlyKey {
 		return nil, err.TypeDBConceptError(err.GET_HAS_WITH_MULTIPLE_FILTERS)
 	}
 
-	return nil, nil
+	s := &common.Thing_GetHas_Req{}
+	if onlyKey {
+		s.KeysOnly = onlyKey
+	}
 
+	if attributeTypes != nil {
+		s.AttributeTypes = *attributeTypes
+	}
+
+	req := &common.Thing_Req{Req: &common.Thing_Req_ThingGetHasReq{ThingGetHasReq: s}}
+	return getThingTx(req, iid), nil
+}
+
+func getThingSetHasReq(iid string, attribute *common.Thing) (*common.Transaction_Req, error) {
+	s := &common.Thing_SetHas_Req{Attribute: attribute}
+	req := &common.Thing_Req{Req: &common.Thing_Req_ThingSetHasReq{ThingSetHasReq: s}}
+	return getThingTx(req, iid), nil
+}
+
+func getThingUnsetHasReq(iid string, attribute *common.Thing) (*common.Transaction_Req, error) {
+	s := &common.Thing_UnsetHas_Req{Attribute: attribute}
+	req := &common.Thing_Req{Req: &common.Thing_Req_ThingUnsetHasReq{ThingUnsetHasReq: s}}
+	return getThingTx(req, iid), nil
+}
+
+func getThingPlayingReq(iid string) (*common.Transaction_Req, error) {
+	s := &common.Thing_GetPlaying_Req{}
+	req := &common.Thing_Req{Req: &common.Thing_Req_ThingGetPlayingReq{ThingGetPlayingReq: s}}
+	return getThingTx(req, iid), nil
+}
+
+func getThinRelationsReq(iid string, roleTypes *[]*common.Type) (*common.Transaction_Req, error) {
+	s := &common.Thing_GetRelations_Req{RoleTypes: *roleTypes}
+	req := &common.Thing_Req{Req: &common.Thing_Req_ThingGetRelationsReq{ThingGetRelationsReq: s}}
+	return getThingTx(req, iid), nil
+}
+
+func getThingDeleteReq(iid string) (*common.Transaction_Req, error) {
+	s := &common.Thing_Delete_Req{}
+	req := &common.Thing_Req{Req: &common.Thing_Req_ThingDeleteReq{ThingDeleteReq: s}}
+	return getThingTx(req, iid), nil
 }
