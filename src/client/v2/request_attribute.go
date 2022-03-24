eea70d2173fe43780f7c9f873d471b2c9952bf31
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-24 15:11:35 +0800

    Fixed all remaining TODO items.

--- src/client/v2/request_attribute.go
+++ src/client/v2/request_attribute.go
@@ -3,15 +3,15 @@ package v2
 import "github.com/marvin-hansen/go-typedb/common"
 
 // Relation
-// 675
+// 719
 
-// FIXME: Attribute_GetOwners_Req doesn't have a thingType field.
 func getAttributeOwnerReq(iid string, ownerType *common.Type) *common.Transaction_Req {
 	s := &common.Attribute_GetOwners_Req{}
 	if ownerType != nil {
-		//s.GetThingType()
+		s.Filter = &common.Attribute_GetOwners_Req_ThingType{
+			ThingType: ownerType,
+		}
 	}
-
 	req := &common.Thing_Req{Req: &common.Thing_Req_AttributeGetOwnersReq{AttributeGetOwnersReq: s}}
 	return getThingTx(req, iid)
 }
--- src/client/v2/request_type_er_relation.go
+++ src/client/v2/request_type_er_relation.go
@@ -1,55 +1,53 @@
 package v2
 
 import (
-	common2 "github.com/marvin-hansen/go-typedb/common"
+	"github.com/marvin-hansen/go-typedb/common"
 	. "github.com/marvin-hansen/go-typedb/src/common"
 )
 
 // EntityType
 // 520
 
-func getCreateEntityReq(label *Label) *common2.Transaction_Req {
-	s := &common2.EntityType_Create_Req{}
-	req := &common2.Type_Req{Req: &common2.Type_Req_EntityTypeCreateReq{EntityTypeCreateReq: s}}
+func getCreateEntityReq(label *Label) *common.Transaction_Req {
+	s := &common.EntityType_Create_Req{}
+	req := &common.Type_Req{Req: &common.Type_Req_EntityTypeCreateReq{EntityTypeCreateReq: s}}
 	return getTypeTx(req, label)
 }
 
 // # RelationType
 // 529
 
-func getRelationTypeCreateReq(label *Label) *common2.Transaction_Req {
-	s := &common2.RelationType_Create_Req{}
-	req := &common2.Type_Req{Req: &common2.Type_Req_RelationTypeCreateReq{RelationTypeCreateReq: s}}
+func getRelationTypeCreateReq(label *Label) *common.Transaction_Req {
+	s := &common.RelationType_Create_Req{}
+	req := &common.Type_Req{Req: &common.Type_Req_RelationTypeCreateReq{RelationTypeCreateReq: s}}
 	return getTypeTx(req, label)
 }
 
-func getRelationTypeGetRelatesReq(label *Label, roleLabel *string) *common2.Transaction_Req {
+func getRelationTypeGetRelatesReq(label *Label, roleLabel *string) *common.Transaction_Req {
 	if roleLabel != nil {
-		s := &common2.RelationType_GetRelatesForRoleLabel_Req{Label: label.GetName()}
-		req := &common2.Type_Req{Req: &common2.Type_Req_RelationTypeGetRelatesForRoleLabelReq{
+		s := &common.RelationType_GetRelatesForRoleLabel_Req{Label: label.GetName()}
+		req := &common.Type_Req{Req: &common.Type_Req_RelationTypeGetRelatesForRoleLabelReq{
 			RelationTypeGetRelatesForRoleLabelReq: s,
 		}}
 		return getTypeTx(req, label)
 	} else {
-		s := &common2.RelationType_GetRelates_Req{}
-		req := &common2.Type_Req{Req: &common2.Type_Req_RelationTypeGetRelatesReq{RelationTypeGetRelatesReq: s}}
+		s := &common.RelationType_GetRelates_Req{}
+		req := &common.Type_Req{Req: &common.Type_Req_RelationTypeGetRelatesReq{RelationTypeGetRelatesReq: s}}
 		return getTypeTx(req, label)
 	}
 }
 
-// FIXME: How to cast string to  isRelationType_SetRelates_Req_Overridden`?
-func getRelationTypeSetRelatesReq(label *Label, roleLabel *string, overriddenLabel *string) *common2.Transaction_Req {
-	s := &common2.RelationType_SetRelates_Req{Label: *roleLabel}
+func getRelationTypeSetRelatesReq(label *Label, roleLabel *string, overriddenLabel *string) *common.Transaction_Req {
+	s := &common.RelationType_SetRelates_Req{}
 	if overriddenLabel != nil {
-		s.Overridden = nil
+		s.Overridden = &common.RelationType_SetRelates_Req_OverriddenLabel{OverriddenLabel: *overriddenLabel}
 	}
-	req := &common2.Type_Req{Req: &common2.Type_Req_RelationTypeSetRelatesReq{RelationTypeSetRelatesReq: s}}
+	req := &common.Type_Req{Req: &common.Type_Req_RelationTypeSetRelatesReq{RelationTypeSetRelatesReq: s}}
 	return getTypeTx(req, label)
-
 }
 
-func getRelationTypeUnsetRelatesReq(label *Label, roleLabel *string) *common2.Transaction_Req {
-	s := &common2.RelationType_UnsetRelates_Req{Label: label.GetName()}
-	req := &common2.Type_Req{Req: &common2.Type_Req_RelationTypeUnsetRelatesReq{RelationTypeUnsetRelatesReq: s}}
+func getRelationTypeUnsetRelatesReq(label *Label, roleLabel *string) *common.Transaction_Req {
+	s := &common.RelationType_UnsetRelates_Req{Label: *roleLabel}
+	req := &common.Type_Req{Req: &common.Type_Req_RelationTypeUnsetRelatesReq{RelationTypeUnsetRelatesReq: s}}
 	return getTypeTx(req, label)
 }
