d9a95427fa3999e21460841a1bf823b4fbf5a60a
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-24 15:21:21 +0800

    Renamed packege imports

--- src/client/v2/request_concept.go
+++ src/client/v2/request_concept.go
@@ -1,55 +1,55 @@
 package v2
 
 import (
-	common2 "github.com/marvin-hansen/go-typedb/common"
+	common "github.com/marvin-hansen/go-typedb/common"
 )
 
 // ConceptManager
 // https://github.com/vaticle/typedb-client-python/blob/master/typedb/common/rpc/request_builder.py
 
 // getConceptTx coverts a ConceptManager_Req into a Transaction_Req
-func getConceptTx(req *common2.ConceptManager_Req) *common2.Transaction_Req {
-	r := &common2.Transaction_Req_ConceptManagerReq{ConceptManagerReq: req}
-	return &common2.Transaction_Req{Req: r}
+func getConceptTx(req *common.ConceptManager_Req) *common.Transaction_Req {
+	r := &common.Transaction_Req_ConceptManagerReq{ConceptManagerReq: req}
+	return &common.Transaction_Req{Req: r}
 }
 
-func getConceptPutEntityReq(label string) *common2.ConceptManager_Req {
-	req := &common2.ConceptManager_Req_PutEntityTypeReq{}
-	req.PutEntityTypeReq = &common2.ConceptManager_PutEntityType_Req{
+func getConceptPutEntityReq(label string) *common.ConceptManager_Req {
+	req := &common.ConceptManager_Req_PutEntityTypeReq{}
+	req.PutEntityTypeReq = &common.ConceptManager_PutEntityType_Req{
 		Label: label,
 	}
-	return &common2.ConceptManager_Req{Req: req}
+	return &common.ConceptManager_Req{Req: req}
 }
 
-func getConceptPutRelationReq(label string) *common2.ConceptManager_Req {
-	req := &common2.ConceptManager_Req_PutRelationTypeReq{}
-	req.PutRelationTypeReq = &common2.ConceptManager_PutRelationType_Req{
+func getConceptPutRelationReq(label string) *common.ConceptManager_Req {
+	req := &common.ConceptManager_Req_PutRelationTypeReq{}
+	req.PutRelationTypeReq = &common.ConceptManager_PutRelationType_Req{
 		Label: label,
 	}
-	return &common2.ConceptManager_Req{Req: req}
+	return &common.ConceptManager_Req{Req: req}
 }
 
-func getConceptPutAttributeReq(label string, valueType common2.AttributeType_ValueType) *common2.ConceptManager_Req {
-	req := &common2.ConceptManager_Req_PutAttributeTypeReq{}
-	req.PutAttributeTypeReq = &common2.ConceptManager_PutAttributeType_Req{
+func getConceptPutAttributeReq(label string, valueType common.AttributeType_ValueType) *common.ConceptManager_Req {
+	req := &common.ConceptManager_Req_PutAttributeTypeReq{}
+	req.PutAttributeTypeReq = &common.ConceptManager_PutAttributeType_Req{
 		Label:     label,
 		ValueType: valueType,
 	}
-	return &common2.ConceptManager_Req{Req: req}
+	return &common.ConceptManager_Req{Req: req}
 }
 
-func getConceptGetThingTypeReq(label string) *common2.ConceptManager_Req {
-	req := &common2.ConceptManager_Req_GetThingTypeReq{}
-	req.GetThingTypeReq = &common2.ConceptManager_GetThingType_Req{
+func getConceptGetThingTypeReq(label string) *common.ConceptManager_Req {
+	req := &common.ConceptManager_Req_GetThingTypeReq{}
+	req.GetThingTypeReq = &common.ConceptManager_GetThingType_Req{
 		Label: label,
 	}
-	return &common2.ConceptManager_Req{Req: req}
+	return &common.ConceptManager_Req{Req: req}
 }
 
-func getConceptGetThingReq(iid string) *common2.ConceptManager_Req {
-	req := &common2.ConceptManager_Req_GetThingReq{}
-	req.GetThingReq = &common2.ConceptManager_GetThing_Req{
+func getConceptGetThingReq(iid string) *common.ConceptManager_Req {
+	req := &common.ConceptManager_Req_GetThingReq{}
+	req.GetThingReq = &common.ConceptManager_GetThing_Req{
 		Iid: []byte(iid),
 	}
-	return &common2.ConceptManager_Req{Req: req}
+	return &common.ConceptManager_Req{Req: req}
 }
