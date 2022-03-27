73c1ea87ed74385fff2fdc65c1b9f02ac50f75ec
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-27 19:59:52 +0800

    Added RunMatchGroupQuery

--- src/client/v2/workflow_query_stream.go
+++ src/client/v2/workflow_query_stream.go
@@ -11,7 +11,8 @@ import "github.com/marvin-hansen/typedb-client-go/common"
 //    oneof res {
 //      Match.ResPart match_res_part = 100;
 //      MatchGroup.ResPart match_group_res_part = 101;
-//      MatchGroupAggregate.ResPart match_group_aggregate_res_part = 102;
+//
+//     MatchGroupAggregate.ResPart match_group_aggregate_res_part = 102;
 //      Insert.ResPart insert_res_part = 103;
 //      Update.ResPart update_res_part = 104;
 //      Explain.ResPart explain_res_part = 105;
@@ -36,3 +37,23 @@ func (c *Client) RunMatchQuery(sessionID, requestId []byte, query string, metada
 
 	return queryResults, nil
 }
+
+func (c *Client) RunMatchGroupQuery(sessionID, requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResults []*common.QueryManager_MatchGroup_ResPart, err error) {
+
+	// Create query request
+	req := getMatchGroupQueryReq(query, options, requestId, metadata)
+
+	// run query
+	streamQuery, queryErr := c.runStreamQuery(sessionID, READ, req, options)
+	if queryErr != nil {
+		return nil, queryErr
+	}
+
+	// extract match results and stuff into queryResults collection
+	for _, item := range streamQuery {
+		queryResults = append(queryResults, item.GetMatchGroupResPart())
+	}
+
+	return queryResults, nil
+}
+
