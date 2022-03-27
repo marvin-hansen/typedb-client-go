9e1544b7aaf746867c9b81cc7d9f7cda9ad6c392
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-27 14:28:17 +0800

    Working on preliminary test for match query

--- test/src/client/workflow_query_match_test.go
+++ test/src/client/workflow_query_match_test.go
@@ -1,7 +1,10 @@
 package client
 
 import (
+	"fmt"
+	"github.com/marvin-hansen/typedb-client-go/common"
 	v2 "github.com/marvin-hansen/typedb-client-go/src/client/v2"
+	"github.com/segmentio/ksuid"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -16,22 +19,50 @@ func TestQueryMatch(t *testing.T) {
 	assert.NotNil(t, c, ClientError)
 
 	// CREATE DATABASE
-	ok, status, err := c.CreateDatabase(dbName)
-	assert.NoError(t, err, "Should be no error")
+	ok, status, createErr := c.CreateDatabase(dbName)
+	assert.NoError(t, createErr, "Should be no error")
 	assert.Equal(t, ok, true, "Should be true")
 	assert.Equal(t, int(status), v2.OK, "Should be OK == 0")
 
 	// TEST MATCH QUERY
 
+	// create requestID
+	requestId := ksuid.New().Bytes()
+
+	query := `match
+				$x sub thing; 
+			get 
+				$x;`
+
+	// create options
+	options := &common.Options{
+		InferOpt:              &common.Options_Infer{Infer: true},
+		ExplainOpt:            &common.Options_Explain{Explain: true},
+		TransactionTimeoutOpt: &common.Options_TransactionTimeoutMillis{TransactionTimeoutMillis: 500},
+	}
+
+	// create metadata
+	metadata := map[string]string{}
+
+	queryResults, queryErr := c.RunMatchQuery(requestId, query, metadata, options)
+	if queryErr != nil {
+		println(fmt.Errorf("could not create transaction: %w", queryErr))
+	}
+
+	assert.NoError(t, queryErr, "Should be no error")
+	assert.NotNil(t, queryResults, "Query should return some results")
+
+	println(queryResults)
+
 	// DELETE DATABASE
-	ok, status, err = c.DeleteDatabase(dbName)
-	assert.NoError(t, err, "Should be no error")
+	ok, status, queryErr = c.DeleteDatabase(dbName)
+	assert.NoError(t, queryErr, "Should be no error")
 	assert.Equal(t, ok, true, "Should be true i.e. exists")
 	assert.Equal(t, int(status), v2.OK, "Should be OK == 0")
 
 	// CHECK DB WAS DELETED
-	existsDatabase, status, err := c.CheckDatabaseExists(dbName)
-	assert.NoError(t, err, "Should be no error")
+	existsDatabase, status, queryErr := c.CheckDatabaseExists(dbName)
+	assert.NoError(t, queryErr, "Should be no error")
 	assert.Equal(t, existsDatabase, false, "Should be true i.e. exists")
 	assert.Equal(t, int(status), int(v2.DBNotExists), "Should be OK == 0")
 }
