b87514bb33ce7b86d2e362f2d4916877fd27272c
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-27 11:55:31 +0800

    Structured query methods into two major blocks: single return type & streaming return type.

--- src/client/v2/workflow_query.go
+++ src/client/v2/workflow_query.go
@@ -4,6 +4,7 @@ package v2
 
 import "github.com/marvin-hansen/typedb-client-go/common"
 
+//runQuery util used by all other specific query methods below
 func (c *Client) runQuery(req []*common.Transaction_Req) (*common.QueryManager_Res, error) {
 
 	// Create a Transaction
@@ -36,6 +37,81 @@ func (c *Client) runQuery(req []*common.Transaction_Req) (*common.QueryManager_R
 	return res, nil
 }
 
+// Methods with singular return type i.e. one request -> one result
+
+func (c *Client) RunDefinedQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResponses *common.QueryManager_Define_Res, err error) {
+
+	// Create a request and attach meta data & request ID
+	r1 := getDefinedQueryReq(query, options, requestId, metadata)
+	// Stuff req into slice/array
+	var req []*common.Transaction_Req
+	req[0] = r1
+
+	// run query
+	res, queryErr := c.runQuery(req)
+	if queryErr != nil {
+		return nil, queryErr
+	} else {
+		return res.GetDefineRes(), nil
+	}
+}
+
+func (c *Client) RunUnDefinedQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResponses *common.QueryManager_Undefine_Res, err error) {
+
+	// Create a request and attach meta data & request ID
+	r1 := getUndefinedQueryReq(query, options, requestId, metadata)
+	// Stuff req into slice/array
+	var req []*common.Transaction_Req
+	req[0] = r1
+
+	// run query
+	res, queryErr := c.runQuery(req)
+	if queryErr != nil {
+		return nil, queryErr
+	} else {
+		return res.GetUndefineRes(), nil
+	}
+}
+
+func (c *Client) RunDeleteQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResponses *common.QueryManager_Delete_Res, err error) {
+
+	// Create a request and attach meta data & request ID
+	r1 := getDeleteQueryReq(query, options, requestId, metadata)
+	// Stuff req into slice/array
+	var req []*common.Transaction_Req
+	req[0] = r1
+
+	// run query
+	res, queryErr := c.runQuery(req)
+	if queryErr != nil {
+		return nil, queryErr
+	} else {
+		return res.GetDeleteRes(), nil
+	}
+}
+
+func (c *Client) RunMatchAggregateQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResponses *common.QueryManager_MatchAggregate_Res, err error) {
+
+	// Create a request and attach meta data & request ID
+	r1 := getMatchAggregateQueryReq(query, options, requestId, metadata)
+	// Stuff req into slice/array
+	var req []*common.Transaction_Req
+	req[0] = r1
+
+	// run query
+	res, queryErr := c.runQuery(req)
+
+	if queryErr != nil {
+		return nil, queryErr
+	} else {
+		return res.GetMatchAggregateRes(), nil
+	}
+}
+
+//
+// Methods with streaming results i.e one initial request -> multiple partial stream results.
+//
+
 func (c *Client) RunInsertQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (matchResponses []*common.QueryManager_Insert_ResPart, recErr error) {
 
 	// Create a request and attach meta data & request ID
@@ -64,40 +140,6 @@ func (c *Client) RunInsertQuery(requestId []byte, query string, metadata map[str
 	// should insert query just return QueryManager_Res ?
 
 	return matchResponses, recErr
-}
-
-func (c *Client) RunDefinedQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResponses *common.QueryManager_Res, err error) {
-
-	// Create a request and attach meta data & request ID
-	r1 := getDefinedQueryReq(query, options, requestId, metadata)
-	// Stuff req into slice/array
-	var req []*common.Transaction_Req
-	req[0] = r1
-
-	// run query
-	queryResponses, queryErr := c.runQuery(req)
-	if queryErr != nil {
-		return nil, queryErr
-	} else {
-		return queryResponses, nil
-	}
-}
-
-func (c *Client) RunUnDefinedQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResponses *common.QueryManager_Res, err error) {
-
-	// Create a request and attach meta data & request ID
-	r1 := getUndefinedQueryReq(query, options, requestId, metadata)
-	// Stuff req into slice/array
-	var req []*common.Transaction_Req
-	req[0] = r1
-
-	// run query
-	queryResponses, queryErr := c.runQuery(req)
-	if queryErr != nil {
-		return nil, queryErr
-	} else {
-		return queryResponses, nil
-	}
 }
 
 func (c *Client) RunUpdateQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResponses *common.QueryManager_Res, err error) {
@@ -134,24 +176,7 @@ func (c *Client) RunExplainQuery(requestId []byte, explainableID int64, metadata
 	}
 }
 
-func (c *Client) RunDeleteQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResponses *common.QueryManager_Res, err error) {
-
-	// Create a request and attach meta data & request ID
-	r1 := getDeleteQueryReq(query, options, requestId, metadata)
-	// Stuff req into slice/array
-	var req []*common.Transaction_Req
-	req[0] = r1
-
-	// run query
-	queryResponses, queryErr := c.runQuery(req)
-	if queryErr != nil {
-		return nil, queryErr
-	} else {
-		return queryResponses, nil
-	}
-}
-
-// Match queries //
+// 	// FIXME: Match queries return type
 //
 // The old golang client implemented match queries using a for-loop over continue flag i.e.
 // for {
@@ -195,24 +220,6 @@ func (c *Client) RunMatchGroupQuery(requestId []byte, query string, metadata map
 
 	// Create a request and attach meta data & request ID
 	r1 := getMatchGroupQueryReq(query, options, requestId, metadata)
-	// Stuff req into slice/array
-	var req []*common.Transaction_Req
-	req[0] = r1
-
-	// run query
-	queryResponses, queryErr := c.runQuery(req)
-
-	if queryErr != nil {
-		return nil, queryErr
-	} else {
-		return queryResponses, nil
-	}
-}
-
-func (c *Client) RunMatchAggregateQuery(requestId []byte, query string, metadata map[string]string, options *common.Options) (queryResponses *common.QueryManager_Res, err error) {
-
-	// Create a request and attach meta data & request ID
-	r1 := getMatchAggregateQueryReq(query, options, requestId, metadata)
 	// Stuff req into slice/array
 	var req []*common.Transaction_Req
 	req[0] = r1
