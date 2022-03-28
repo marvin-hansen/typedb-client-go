5df8b81a7ce4b7c1d57c8122b00dd090ca02dcac
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-28 11:45:58 +0800

    Added error details to runQuery

--- src/client/v2/workflow_query_util.go
+++ src/client/v2/workflow_query_util.go
@@ -1,6 +1,9 @@
 package v2
 
-import "github.com/marvin-hansen/typedb-client-go/common"
+import (
+	"fmt"
+	"github.com/marvin-hansen/typedb-client-go/common"
+)
 
 //runQuery util used by all other single return value query methods
 func (c *Client) runQuery(req []*common.Transaction_Req) (*common.QueryManager_Res, error) {
@@ -8,28 +11,29 @@ func (c *Client) runQuery(req []*common.Transaction_Req) (*common.QueryManager_R
 	// Create a Transaction
 	tx, txErr := c.client.Transaction(c.ctx)
 	if txErr != nil {
-		return nil, txErr
+		return nil, fmt.Errorf("could not create transaction: %w", txErr)
 	}
 
 	// Send request through
 	sendErr := tx.Send(getTransactionClient(req))
 	if sendErr != nil {
-		return nil, sendErr
-	}
-
-	// Close send
-	txCloseErr := tx.CloseSend()
-	if txCloseErr != nil {
-		return nil, txCloseErr
+		return nil, fmt.Errorf("could not send transaction to server: %w", sendErr)
 	}
 
 	// get return value
 	recv, recErr := tx.Recv()
 	if recErr != nil {
-		return nil, recErr
+		return nil, fmt.Errorf("could not receive query response: %w", recErr)
 	}
+
 	// Extract query result
 	res := recv.GetRes().GetQueryManagerRes()
+
+	// close transaction
+	closErr := tx.CloseSend()
+	if closErr != nil {
+		return nil, fmt.Errorf("could not close query transaction: %w", closErr)
+	}
 
 	return res, nil
 }
