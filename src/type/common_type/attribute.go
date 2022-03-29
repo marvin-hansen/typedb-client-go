0a438e8229b44a34f02419f0699dce174029e72f
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-29 16:30:03 +0800

    Added attribute type

--- src/type/common_type/attribute.go
+++ src/type/common_type/attribute.go
@@ -1,1 +1,37 @@
 package common_type
+
+type Attribute struct {
+	Value ValueItem
+}
+
+type ValueItem[T Value] struct {
+	Item T
+}
+
+func (i ValueItem[T]) GetValue() (item T) {
+	return i.Item
+}
+
+type Value interface {
+	StringValue | BooleanValue | LongValue | DoubleValue | DateTimeValue
+}
+
+type StringValue struct {
+	Value string
+}
+
+type BooleanValue struct {
+	Value bool
+}
+
+type LongValue struct {
+	Value int64
+}
+
+type DoubleValue struct {
+	Value float64
+}
+
+type DateTimeValue struct {
+	Value int64
+}
