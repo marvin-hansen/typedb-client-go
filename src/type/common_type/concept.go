115b9f86f15506b874d1fa3806b1fff5f6e32c14
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-29 17:28:47 +0800

    Updated conept & conceptMap

--- src/type/common_type/concept.go
+++ src/type/common_type/concept.go
@@ -4,3 +4,13 @@ package common_type
 type Concept[T Value] struct {
 	Concept T
 }
+
+type ConceptMap[V Value] struct {
+	Map          map[string]Concept[V] `json:"map,omitempty"`
+	Explainables Explainables          `json:"explainables,omitempty"`
+}
+
+type ConceptMapGroup[V Value] struct {
+	Owner       Concept[V]      `json:"owner,omitempty"`
+	ConceptMaps []ConceptMap[V] `json:"concept_maps,omitempty"`
+}
