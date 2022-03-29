f4a55839c5c9005119fbc8be32214f7db56f3037
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-29 17:28:23 +0800

    Added Explainables type

--- src/type/common_type/explainable.go
+++ src/type/common_type/explainable.go
@@ -4,3 +4,13 @@ type Explainable struct {
 	Conjunction string `json:"conjunction,omitempty"`
 	ID          int64  `json:"id,omitempty"`
 }
+
+type Owned struct {
+	Owned map[string]Explainable `json:"owned,omitempty"`
+}
+
+type Explainables struct {
+	Relations  map[string]Explainable `json:"relations,omitempty"`
+	Attributes map[string]Explainable `json:"attributes,omitempty"`
+	Ownerships map[string]Owned       `json:"ownerships,omitempty"`
+}
