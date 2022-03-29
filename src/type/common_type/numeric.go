e7a85101427bda6b520c19fb4ab9ca354171a1a1
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-29 17:28:34 +0800

    Added NumericType

--- src/type/common_type/numeric.go
+++ src/type/common_type/numeric.go
@@ -1,1 +1,14 @@
 package common_type
+
+type Numeric[T NumericType] struct {
+	Value T `json:"value,omitempty"`
+}
+
+type NumericType interface {
+	int64 | float64 | bool
+}
+
+type NumericGroup[T NumericType, V Value] struct {
+	Owner   Concept[V] `json:"owner,omitempty"`
+	Numeric Numeric[T] `json:"numeric,omitempty"`
+}
