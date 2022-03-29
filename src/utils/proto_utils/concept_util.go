9cc1e36aa1710ed6a20f97540875b212c716830e
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-29 19:13:36 +0800

    Added proto utils

---
+++ src/utils/proto_utils/concept_util.go
@@ -0,0 +1,11 @@
+package proto_utils
+
+import "github.com/marvin-hansen/typedb-client-go/common"
+
+func ConvertProtoToConcept(conceptMap *common.Concept) {
+
+}
+
+func ConvertProtoToConceptMap(conceptMap *common.ConceptMap) {
+
+}
---
+++ src/utils/proto_utils/option_util.go
@@ -0,0 +1,14 @@
+package proto_utils
+
+import (
+	"github.com/marvin-hansen/typedb-client-go/common"
+	"github.com/marvin-hansen/typedb-client-go/src/type/common_type"
+)
+
+func ConcertOptionToProto(options *common.Options) {
+
+}
+
+func ConvertProtoToOption(options common_type.Options) {
+
+}
