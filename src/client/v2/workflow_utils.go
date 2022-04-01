2cdddac581092ab76d83afca1c0598ce63e9662b
Marvin Hansen <marvin.hansen@gmail.com>
2022-04-01 14:57:39 +0800

    Merged dbg tools & utils

--- src/client/v2/workflow_utils.go
+++ src/client/v2/workflow_utils.go
@@ -6,7 +6,25 @@ import (
 	"fmt"
 	"github.com/marvin-hansen/typedb-client-go/common"
 	"github.com/segmentio/ksuid"
+	"google.golang.org/grpc"
+	"log"
 )
+
+func checkPrintErr(err error, errorMsg string) {
+	if err != nil {
+		log.Println("Error:", err.Error())
+		log.Println("Error Message: ", errorMsg)
+	}
+}
+
+func checkConnection(conn *grpc.ClientConn, err error) {
+	if err != nil {
+		checkPrintErr(err, connErr)
+	}
+	if conn == nil {
+		log.Fatal(connErr)
+	}
+}
 
 // dbCheck checks if a DB with the name exists
 func (c *Client) dbCheck(dbName string) (err error) {
