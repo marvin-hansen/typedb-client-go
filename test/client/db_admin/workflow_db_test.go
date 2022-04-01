5eca3cd4dbfded306a2546a6cd12ec46e0536a83
Marvin Hansen <marvin.hansen@gmail.com>
2022-04-01 15:40:39 +0800

    Added comments to test

--- test/client/db_admin/workflow_db_test.go
+++ test/client/db_admin/workflow_db_test.go
@@ -12,6 +12,7 @@ import (
 const dbName = utils.DBName
 
 func TestNewClient(t *testing.T) {
+	// create new client with default localhost config
 	conf := v2.NewLocalConfig(dbName)
 	c, cancel := v2.NewClient(conf)
 	defer cancel()
@@ -55,7 +56,7 @@ func TestDeleteDatabase(t *testing.T) {
 	actual := existsDatabase
 	assert.Equal(t, expected, actual, "Should be true i.e. exists")
 
-	// Delete if exists.
+	// Delete DB if exists.
 	// Notice, DeleteDatabase returns true if the DB doesn't exist without being deleted b/c it's already gone
 	// AND returns true when the DB actually got deleted.
 	ok, err := c.DBManager.DeleteDatabase(dbName)
