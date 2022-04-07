2fc28dafc1fc1b7f4a797d0f30bfd78fc002a2a4
Marvin Hansen <marvin.hansen@gmail.com>
2022-04-07 11:36:13 +0800

    Coe formatting

--- src/client/v2/manager_session_monitor.go
+++ src/client/v2/manager_session_monitor.go
@@ -4,61 +4,38 @@ import (
 	"context"
 	"fmt"
 	"github.com/marvin-hansen/typedb-client-go/src/client/v2/requests"
-	"time"
 )
 
 func (s SessionManager) startMonitorSession(sessionID []byte) {
+	// How do I track each goRoutine per session
 
 }
 
 func (s SessionManager) stopMonitorSession(sessionID []byte) {
-
+	// How do I call the correct cancle function to stop the GoRoutine matchin the session?
 }
 
-func (s SessionManager) heartbeat(ctx context.Context, sessionID []byte) context.CancelFunc {
+func (s SessionManager) runHeartbeat(ctx context.Context, sessionID []byte) context.CancelFunc {
 
 	// Create a new context, with its cancellation function cfrom the original context
 	ctx, cancel := context.WithCancel(ctx)
-
 	go func() {
 		select {
 		case <-ctx.Done():
 			fmt.Println("Stopped monitoring session: ")
-
 		default:
-			err := s.sendTrackHeartbeat(ctx, sessionID)
+			err := s.sendPulseRequest(sessionID)
 			// If this operation returns an error
 			// cancel all operations using this local context created above
 			if err != nil {
 				cancel()
 			}
-
 			fmt.Println("done")
 		}
 	}()
 
 	// return cancel function for callsite to close at a later stage
 	return cancel
-}
-
-func (s SessionManager) sendTrackHeartbeat(ctx context.Context, sessionID []byte) error {
-
-	select {
-	// If we do not get a response within 500 ms, we consider it a timeout
-	case <-time.After(500 * time.Millisecond):
-		err := s.sendPulseRequest(sessionID)
-		if err != nil {
-			return fmt.Errorf("heaetbeart  timed out  %w", err)
-		}
-
-		fmt.Println("done")
-		return nil
-
-	case <-ctx.Done():
-		fmt.Println("Stopped heartbeat")
-		return nil
-
-	}
 }
 
 func (s SessionManager) sendPulseRequest(sessionID []byte) error {
